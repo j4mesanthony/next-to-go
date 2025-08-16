bbfdc5556cbf5562e96b02b3c5bf089ce1d8ba65
James Anthony <janthony@greenleaf.com.au>
2025-08-16 21:39:20 +1000

    feat: Added loading spinner.

--- src/features/nextToGo/NextToGoView.vue
+++ src/features/nextToGo/NextToGoView.vue
@@ -12,7 +12,8 @@
         <div className="flex flex-col sm:flex-row gap-2 sm:gap-1 items-center grow">
             <h1
                 className="h-12 bg-gray-800 text-white text-md p-3 font-bold uppercase grow w-full sm:max-w-2/3 text-left flex flex-row justify-between">
-                {{ isLoading ? "loading..." : "Next to go racing" }}
+                Next to go racing
+                <img v-if="isLoading" src="../../assets/spinner.svg" alt="Loading..." className="animate-spin" />
             </h1>
             <FilterOptions v-model="selectedFilters" :options="RACING_CATEGORIES_LIST" />
         </div>
